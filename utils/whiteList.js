f76a58ffc30da8e85cdf75cc3ce3f452fb63d489
Luiz Calaça <luizcalaca@gmail.com>
2022-06-12 00:14:16 -0300

    feat: adding cors options

---
+++ utils/whiteList.js
@@ -0,0 +1,12 @@
+const whitelist = ['http://localhost:3000', 'http://localhost:3001']
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+module.exports = corsOptions
